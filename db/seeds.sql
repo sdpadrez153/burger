e5d6f8d01a9beea92662f45248cae68fc86d50d3
sdpadrez153 <dmata62087@gmail.com>
2019-02-28 19:31:53 -0700

    changes1

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double All American Burger');
+INSERT INTO burgers (burger_name) VALUES ('PB & J Burger');
---
+++ server.js
@@ -0,0 +1,38 @@
+var Sequelize = require('sequelize'), connection;
+if (process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new Sequelize("burgers_db", "root", "root", {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3306'
+  })
+}
+
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
